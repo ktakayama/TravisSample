9785547ae67f3e4ab7cfb5e0fb88cc93f5afbe8e
Kyosuke Takayama <loiseau@gmail.com>
2014-09-03 11:55:29 +0900

    テストコードを追加

---
+++ TravisSampleTests/TSJsonModelTests.m
@@ -0,0 +1,30 @@
+//
+//  TSJsonModelTests.m
+//  TravisSample
+//
+//  Created by Kyosuke Takayama on 2014/09/03.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import "TSJsonModel.h"
+
+@interface TSJsonModelTests : XCTestCase
+
+@end
+
+@implementation TSJsonModelTests
+
+- (void) testCreateObject {
+   TSJsonModel *model = [[TSJsonModel alloc] initWithDictionary:@{
+                                                          @"id": @"1001",
+                                                        @"name": @"なまえ",
+                                                     @"comment": @"こめんと",
+   }];
+
+   XCTAssertEqualObjects(model.identifier, @"1001");
+   XCTAssertEqualObjects(model.name,       @"なまえ");
+   XCTAssertEqualObjects(model.comment,    @"こめんと");
+}
+
+@end
